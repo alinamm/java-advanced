335199d5808c03a6a456110812df9befd63b38a6
Alina <307950@niuitmo.ru>
2022-02-24 14:30:47 +0300

    added

---
+++ java-solutions/info/kgeorgiy/ja/mustafina/arrayset/ArraySet.java
@@ -0,0 +1,134 @@
+package info.kgeorgiy.ja.mustafina.arrayset;
+
+import java.util.*;
+
+public class ArraySet<E> extends AbstractSet<E> implements SortedSet<E> {
+
+    private final ArrayList<E> array;
+    private final Comparator<E> comparator;
+
+    public ArraySet() {
+        this(null, null);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return array.iterator();
+    }
+
+    public ArraySet(Comparator<E> comparator) {
+        this(null, comparator);
+    }
+
+    public ArraySet(Collection<E> collection) {
+        this(collection, null);
+    }
+
+    public ArraySet(Collection<E> collection, Comparator<E> comparator) {
+        this.comparator = comparator;
+        if (collection != null) {
+            TreeSet<E> treeSet = new TreeSet<>(comparator);
+            treeSet.addAll(collection);
+            array = new ArrayList<>(treeSet);
+        } else {
+            array = new ArrayList<>(new TreeSet<>());
+        }
+    }
+
+    @Override
+    public int size() {
+        return array.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.size() == 0;
+    }
+
+    @Override
+    public boolean contains(Object e) {
+        return findElement((E) e) >= 0;
+    }
+
+    @Override
+    public Comparator<E> comparator() {
+        return comparator;
+    }
+
+    private int findElement(E element) {
+        return Collections.binarySearch(array, element, comparator);
+    }
+
+    @Override
+    public SortedSet<E> headSet(E toElement) {
+        return getSet(null, toElement);
+    }
+
+    @Override
+    public SortedSet<E> subSet(E fromElement, E toElement) {
+        if (comparator.compare(fromElement, toElement) > 0) {
+            throw new IllegalArgumentException();
+        }
+        return getSet(fromElement, toElement);
+    }
+
+    @Override
+    public SortedSet<E> tailSet(E fromElement) {
+        return getSet(fromElement, null);
+    }
+
+    private SortedSet<E> getSet(E fromElement, E toElement) {
+        int toIndex;
+        if (toElement != null) {
+            toIndex = findElement(toElement);
+            if (toIndex < 0) {
+                toIndex = Math.abs(toIndex) - 1;
+            }
+        } else {
+            toIndex = array.size();
+        }
+        int fromIndex;
+        if (fromElement != null) {
+            fromIndex = findElement(fromElement);
+            if (fromIndex < 0) {
+                fromIndex = Math.abs(fromIndex) - 1;
+            }
+        } else {
+            fromIndex = 0;
+        }
+        List<E> subSet = array.subList(fromIndex, toIndex);
+        return new ArraySet<>(subSet, comparator);
+    }
+
+    @Override
+    public E last() {
+        return firstOrLastElement(array.size() - 1);
+    }
+
+    @Override
+    public E first() {
+        return firstOrLastElement(0);
+    }
+
+    private E firstOrLastElement(int index) {
+        if (array.size() == 0) {
+            throw new NoSuchElementException();
+        }
+        return array.get(index);
+    }
+
+    @Override
+    public boolean add(E e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+}
