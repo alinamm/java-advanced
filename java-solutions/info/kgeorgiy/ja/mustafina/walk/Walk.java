0df64d6ec1ec9b3d3b8f773610c4003d15edf09e
Alina <307950@niuitmo.ru>
2022-03-03 16:18:25 +0300

    file changed

--- java-solutions/info/kgeorgiy/ja/mustafina/walk/Walk.java
+++ java-solutions/info/kgeorgiy/ja/mustafina/walk/Walk.java
@@ -11,6 +11,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class Walk {
     public static String ERROR_WHILE_READING_FILE = "0".repeat(40);
+    public static Integer SIZE_OF_BUFFER = 8192;
 
     public static void main(String[] args) {
         if (args == null || args.length != 2 || args[0] == null || args[1] == null) {
@@ -36,28 +37,28 @@ public class Walk {
             }
         }
 
-        // :NOTE: разнести исключения reader и Writer
-        try (BufferedReader reader = Files.newBufferedReader(Path.of(args[0]));
-             BufferedWriter writer = Files.newBufferedWriter(Path.of(args[1]))) {
-
-            String path;
-            while ((path = reader.readLine()) != null) {
-                StringBuilder result = new StringBuilder();
-                MessageDigest md = MessageDigest.getInstance("SHA-1");
-                Path filePath = null;
-                try {
-                    filePath = Path.of(path);
-                } catch (InvalidPathException e) {
-                    result.append(ERROR_WHILE_READING_FILE);
-                }
-                if (filePath != null && Files.notExists(filePath)) {
-                    result.append(ERROR_WHILE_READING_FILE);
-                }
-                if (result.length() == 0) {
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(args[0]))) {
+            try (BufferedWriter writer = Files.newBufferedWriter(Path.of(args[1]))) {
+                String path;
+                while ((path = reader.readLine()) != null) {
+                    StringBuilder result = new StringBuilder();
+                    MessageDigest md = MessageDigest.getInstance("SHA-1");
+                    Path filePath;
+                    try {
+                        filePath = Path.of(path);
+                    } catch (InvalidPathException e) {
+                        result.append(ERROR_WHILE_READING_FILE);
+                        write(writer, result, path);
+                        continue;
+                    }
+                    if (Files.notExists(filePath)) {
+                        result.append(ERROR_WHILE_READING_FILE);
+                        write(writer, result, path);
+                        continue;
+                    }
                     try (InputStream inputStream = Files.newInputStream(Path.of(path))) {
                         int n = 0;
-                        // :NOTE: move 8000, 8192
-                        byte[] buffer = new byte[8000];
+                        byte[] buffer = new byte[SIZE_OF_BUFFER];
                         while (n != -1) {
                             n = inputStream.read(buffer);
                             if (n > 0) {
@@ -67,32 +68,36 @@ public class Walk {
                         for (byte b : md.digest()) {
                             result.append(String.format("%02x", b));
                         }
-                        // :NOTE: NPE, Security, UnsupportedOperation
-                    } catch (IllegalArgumentException | UnsupportedOperationException | IOException | SecurityException | NullPointerException e) {
+                    } catch (IllegalArgumentException | IOException e) {
                         result.append(ERROR_WHILE_READING_FILE);
                     }
+                    write(writer, result, path);
                 }
-                // :NOTE: \n
-                writer.write(result + " " + path + "\n");
             }
         } catch (IOException e) {
-            System.err.println("Input or output exception" + " " + e.getMessage());
+            System.err.println("Input exception" + " " + e.getMessage());
         } catch (NoSuchAlgorithmException e) {
             System.err.println("Failed to find algorithm" + " " + e.getMessage());
-            // :NOTE: security exception
-        } catch (SecurityException e) {
-            System.err.println("Security exception" + " " + e.getMessage());
         }
     }
 
     public static boolean checkPath(String path, String message) {
         try {
-            // :NOTE: unused variable
-            Path filePath = Path.of(path);
+            Path.of(path);
         } catch (InvalidPathException e) {
             System.err.println("Invalid path to " + message + " file" + " " + e.getMessage());
             return true;
         }
         return false;
     }
+
+    public static void write(BufferedWriter writer, StringBuilder result, String path) {
+        try {
+            writer.write(result + " " + path);
+            writer.newLine();
+        } catch (IOException e) {
+            System.err.println("Output exception" + " " + e.getMessage());
+        }
+    }
 }
+
