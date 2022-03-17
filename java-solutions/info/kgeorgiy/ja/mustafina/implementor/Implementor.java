4c46a85e694a99f075c906ee761222f87fca1bc3
Kamenev Yury <kamenev.yury@huawei.com>
2022-03-17 16:23:24 +0300

    Comments

--- java-solutions/info/kgeorgiy/ja/mustafina/implementor/Implementor.java
+++ java-solutions/info/kgeorgiy/ja/mustafina/implementor/Implementor.java
@@ -28,6 +28,7 @@ public class Implementor implements Impler {
     @Override
     public void implement(Class<?> token, Path root) throws ImplerException {
         int modifiers = token.getModifiers();
+        // :NOTE: check interface
         if (Modifier.isPrivate(modifiers) || Modifier.isFinal(modifiers)) {
             throw new ImplerException("Invalid token");
         }
@@ -37,6 +38,7 @@ public class Implementor implements Impler {
             Path file = path.resolve(token.getSimpleName() + "Impl.java");
             try (BufferedWriter bufferedWriter = Files.newBufferedWriter(file)) {
                 bufferedWriter.write(token.getPackageName().isEmpty() ? "" : ("package " + token.getPackageName() + ";" + NEW_LINE)
+                        // :NOTE: simpleName instead of canonicalName
                         + " public class " + token.getSimpleName() + "Impl " + "implements " + token.getCanonicalName()
                         + "{" + NEW_LINE + methods(token) + NEW_LINE + "}");
             } catch (IOException e) {
@@ -47,10 +49,14 @@ public class Implementor implements Impler {
         }
     }
 
+    // :NOTE: too long and complicated one-liner method
     private String methods(Class<?> token) {
         return mapAndCollect(token.getMethods(),
+                // :NOTE: private methods
                 s -> TAB + Modifier.toString(s.getModifiers() & ~Modifier.ABSTRACT & ~Modifier.TRANSIENT) + " "
                         + s.getReturnType().getCanonicalName() + " " + s.getName()
+                        // :NOTE: private return type, parameters
+                        // :NOTE: missing exceptions
                         + "(" + mapAndCollect(s.getParameters(), m -> m.getType().getCanonicalName()
                         + " " + m.getName(), ", ") + ") " + "{" + NEW_LINE + TAB + TAB + "return "
                         + (s.getReturnType() == boolean.class ? true : s.getReturnType() == void.class ? ""
@@ -60,4 +66,13 @@ public class Implementor implements Impler {
     private <R> String mapAndCollect(R[] array, Function<R, String> function, String s) {
         return Arrays.stream(array).map(function).collect(Collectors.joining(s));
     }
+
+    // :NOTE:
+//    private class A {}
+//
+//    public interface I {
+//        private A foo(A a) {
+//            return a;
+//        }
+//    }
 }
